7b41b8af3c96c7244e3b1f0e53c72b73afcf29d3
MemonaKhan <you@example.comsuhail1975pk@yahoo.co.uk>
2020-06-17 22:54:43 +0500

    started working

---
+++ app.js
@@ -0,0 +1,21 @@
+// // Assignment # 21-25
+
+// // Task # 1
+
+// var firstName = prompt("Enter your First Name","First Name");
+// var lastName = prompt("Enter your Last Name","Last Name");
+// var fullName = firstName + " " + lastName;
+// alert("Hello " + fullName + "\n" + "Have a Good Day!");
+
+
+// // Task # 2
+
+// var favMobModel = prompt("Enter your favorite mobile phone model");
+// var strLen = favMobModel.length;
+// document.write("My favorite phone is: "+ favMobModel);
+// document.write("<br>Length of String: "+ strLen);
+
+
+// // Task # 3
+
+
